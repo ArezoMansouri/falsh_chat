1de51c47b39b6270e3f65ccc07e7a2ed993da4f0
ArezoMansouri <arezomansouri2000@gmail.com>
2023-02-13 23:41:27 +0430

    Updated

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat_starting_project/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat_starting_project/constants.dart';
 
@@ -19,7 +21,9 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: const Icon(Icons.logout),
               onPressed: () {
-                // Implement logout functionality
+                Navigator.pop(context);
+                AuthService().signOut();
+
               }),
         ],
         title: const Text('⚡ ️Chat'),
